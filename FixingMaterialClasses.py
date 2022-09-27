6ba82ba0ee0ddffe9b14b29c587c0b9c97069528
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 20:53:52 +0300

    готовый вариант в игру в реальность

--- FixingMaterialClasses.py
+++ FixingMaterialClasses.py
@@ -1,3 +1,5 @@
+import random
+
 class Man:
     def __init__(self, name):
         self.name = name
@@ -11,12 +13,20 @@ class Man:
                                     fullness, self.food, self.money)
 
     def eat(self):
-        if self.food > 10:
+        if self.food >= 10:
             print('{} поел'.format(self.name))
             self.fullness += 10
             self.food -= 10
         else:
             print('{} нет еды'.format(self.name))
+
+    def shopping_for_food(self):
+        if self.money >= 50:
+            print('{} сходил в магаз за сосисками'.format(self.name))
+            self.food += 50
+            self.money -= 50
+        else:
+            print('У персонажа {} нет денег'.format(self.name))
 
     def work(self):
         print('{} сходил на работу'.format(self.name))
@@ -27,13 +37,27 @@ class Man:
         print('{} поиграл'.format(self.name))
         self.fullness -= 10
 
+    def act(self):
+        if self.fullness <= 0:
+            print ('{} умер'.format(self.name))
+            return
+        dice = random.randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.food < 10:
+            self.shopping_for_food()
+        elif self.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.play_DOTA()
 
 ivan = Man(name='Иван')
-print(ivan)
 
-ivan.eat()
-print(ivan)
-ivan.work()
-print(ivan)
-ivan.play_DOTA()
-print(ivan)
+for day in range(1,21):
+    print('============={}==============='.format(day))
+    ivan.act()
+    print(ivan)
