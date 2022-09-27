9bb639ff5e6ef25f13b7ae073bf529d656b3cfe1
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 21:57:38 +0300

    сделал очень крутую фишку, где можно добавлять в объект дома объекты люди и они живут, работают, и едят и следят,чтобы всего хватало

--- FixingMaterialClasses.py
+++ FixingMaterialClasses.py
@@ -4,37 +4,36 @@ class Man:
     def __init__(self, name):
         self.name = name
         self.fullness = 50
-        self.food = 50
-        self.money = 0
+        self.house = None
 # степень сытости, еда, деньги
 
     def __str__(self):
-        return 'Я - {} сытость - {} еды - {} денег - {}'.format(self.name, self.
-                                    fullness, self.food, self.money)
+        return 'Я - {} сытость - {}'.format(self.name, self.
+                                    fullness)
 
     def eat(self):
-        if self.food >= 10:
+        if self.house.food >= 10:
             print('{} поел'.format(self.name))
             self.fullness += 10
-            self.food -= 10
+            self.house.food -= 10
         else:
             print('{} нет еды'.format(self.name))
 
     def shopping_for_food(self):
-        if self.money >= 50:
+        if self.house.money >= 50:
             print('{} сходил в магаз за сосисками'.format(self.name))
-            self.food += 50
-            self.money -= 50
+            self.house.food += 50
+            self.house.money -= 50
         else:
             print('У персонажа {} нет денег'.format(self.name))
 
     def work(self):
         print('{} сходил на работу'.format(self.name))
-        self.money += 50
+        self.house.money += 50
         self.fullness -= 10
 
-    def play_DOTA(self):
-        print('{} поиграл'.format(self.name))
+    def watching_tv(self):
+        print('{} смотрел телевизор'.format(self.name))
         self.fullness -= 10
 
     def act(self):
@@ -44,20 +43,42 @@ class Man:
         dice = random.randint(1, 6)
         if self.fullness < 20:
             self.eat()
-        elif self.food < 10:
+        elif self.house.food < 10:
             self.shopping_for_food()
-        elif self.money < 50:
+        elif self.house.money < 50:
             self.work()
         elif dice == 1:
             self.work()
         elif dice == 2:
             self.eat()
         else:
-            self.play_DOTA()
+            self.watching_tv()
+    def go_into_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        termcolor.cprint('{} заехал на хату'.format(self.name), color='red')
+class House:
+    def __init__(self):
+        self.money = 0
+        self.food = 50
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(self.food
+                                                                  , self.money)
+citizens = [
+Man(name='Иван'),
+Man(name='Петя'),
+Man(name='Кенни')
+]
 
-ivan = Man(name='Иван')
+my_sweet_house = House()
+for citizen in citizens:
+    citizen.go_into_house(house=my_sweet_house)
 
-for day in range(1,21):
+for day in range(1,366):
     termcolor.cprint('============={}==============='.format(day), color='yellow')
-    ivan.act()
-    print(ivan)
+    for citizen in citizens:
+        citizen.act()
+    print('=========================')
+    for citizen in citizens:
+        print(citizen)
+    print(my_sweet_house)
